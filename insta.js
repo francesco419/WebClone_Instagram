f9f1f6b675a5ab2f798dc133fe4c7e4f9943f012
LoSH <84317858+francesco419@users.noreply.github.com>
2022-03-08 23:06:31 +0900

    Add files via upload

--- insta.js
+++ insta.js
@@ -1,18 +1,21 @@
+'use strict';
+
 const cd = document.getElementById('clickme1');
 const id = document.getElementById('popup1');
 const cc = document.getElementById("ccc");
 const aa = document.getElementById("popup2")
 
 cc.onmouseover=function(){
-    document.getElementById("cimage2").style.display="block";
+    document.getElementById("cimage2").style.display="inline";
 }
 cc.onmouseout=function(){
     document.getElementById("cimage2").style.display="none";
 }
 
 cd.onmouseover=function(){
-    document.getElementById("cimage1").style.display="block";
+    document.getElementById("cimage1").style.display="inline";
 }
+
 cd.onmouseout=function(){
     document.getElementById("cimage1").style.display="none";
 }
@@ -20,16 +23,19 @@ cd.onmouseout=function(){
 cd.addEventListener('click',function(){
     if(id.style.display=="none"){
         id.style.display="inline";
-    }else{
-        id.style.display="none";
     }
+})
+id.addEventListener('click',function(){
+    id.style.display="none";
 })
 
 cc.addEventListener('click',function(){
     if(aa.style.display=="none"){
         aa.style.display="inline";
-    }else{
-        aa.style.display="none";
     }
+})
+
+aa.addEventListener('click',function(){
+    aa.style.display="none";
 })
 
